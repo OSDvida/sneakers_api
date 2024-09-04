9bb4ffe2ec48a4b82a94159f9a752e4273df96ad
Miguel <miguelvgioia@gmail.com>
2024-09-04 16:14:07 -0300

    criado entidade de brand

---
+++ src/main/java/com/sneaker_store/domain/Brand.java
@@ -0,0 +1,66 @@
+package com.sneaker_store.domain;
+
+import jakarta.persistence.*;
+
+public class Brand {import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+    @Entity
+    public class Brand {
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE)
+        @Column(name = "id", nullable = false)
+        private Long id;
+
+        @Column(name = "name", nullable = false, unique = true)
+        private String name;
+
+        @Column(name = "country_of_origin")
+        private String countryOfOrigin;
+
+        @Column(name = "description")
+        private String description;
+
+        public Brand() {
+        }
+
+        public Brand(Long id, String name, String countryOfOrigin) {
+            this.id = id;
+            this.name = name;
+            this.countryOfOrigin = countryOfOrigin;
+            this.setDescription();
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCountryOfOrigin() {
+            return countryOfOrigin;
+        }
+
+        public void setCountryOfOrigin(String countryOfOrigin) {
+            this.countryOfOrigin = countryOfOrigin;
+        }
+        public void setDescription(String description) {
+            this.description (); = description;
+    }
+
+}
+
+}
