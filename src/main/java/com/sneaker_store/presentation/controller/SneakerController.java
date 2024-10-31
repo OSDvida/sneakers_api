a0d037c4efd0b775946fce313ceac55f8d7da3f8
ANG7CA <ang7ca@bosch.com>
2024-10-31 07:25:48 -0300

    Finish project to presentation

--- src/main/java/com/sneaker_store/presentation/controller/SneakerController.java
+++ src/main/java/com/sneaker_store/presentation/controller/SneakerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.s3.endpoints.internal.Value;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -27,17 +28,24 @@ public class SneakerController {
     }
 
     @PostMapping()
-    public ResponseEntity<?> createSneaker(@RequestBody SneakerDTO dto){
-        sneakerService.create(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    public ResponseEntity<Long> createSneaker(@RequestBody SneakerDTO dto){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(sneakerService.create(dto));
     }
 
-    @PostMapping("/{id}/upload-image")
-    public ResponseEntity<String> uploadImage(@RequestParam("file")MultipartFile file){
+    @PostMapping(value = "/{id}/upload-image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadImage(@PathVariable Long id, @RequestParam("file") MultipartFile file) throws IOException {
          if(file.isEmpty()){
              return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("O arquivo está vazio.");
          }
+        sneakerService.addImageToSneaker(id, file);
 
-         return  null;
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteSneaker(@PathVariable Long id){
+        sneakerService.deleteSneaker(id);
+       return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
--- src/main/java/com/sneaker_store/presentation/mapper/SneakerMapper.java
+++ src/main/java/com/sneaker_store/presentation/mapper/SneakerMapper.java
@@ -5,12 +5,21 @@ import com.sneaker_store.presentation.dto.SneakerDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
 
 import java.util.List;
+import java.util.Objects;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface SneakerMapper {
     SneakerDTO toDTO(Sneaker sneaker);
     List<SneakerDTO> toDTOList(List<Sneaker> sneakers);
+
+    @Mapping(source = "imagePath", target = "imagePath", qualifiedByName = "mapImagePath")
     Sneaker toEntity(SneakerDTO sneakerDTO);
+
+    @Named("mapImagePath")
+    default String mapDefaultImagePath(String imagePath){
+        return Objects.isNull(imagePath) ? "" : imagePath;
+    }
 }
